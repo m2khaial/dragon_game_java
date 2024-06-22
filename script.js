1a0f7390d0282a33fa13e034c6e71946e31b42f0
m2khaial <156560094+m2khaial@users.noreply.github.com>
2024-06-22 08:15:18 +0000

    Added game outcomes.

--- script.js
+++ script.js
@@ -208,4 +208,46 @@ function buyWeapon() {
     }
   }
 
+  function getMonsterAttackValue(level) {
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    console.log(hit);
+    return hit > 0 ? hit : 0;
+  }
+  
+  function isMonsterHit() {
+    return Math.random() > .2 || health < 20;
+  }
+  
+  function dodge() {
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+  }
+  
+  function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+  }
+  
+  function lose() {
+    update(locations[5]);
+  }
+  
+  function winGame() {
+    update(locations[6]);
+  }
+  
+  function restart() {
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ["stick"];
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+    goTown();
+  }
+  
   
